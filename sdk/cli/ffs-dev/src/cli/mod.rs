9c5ea104f100b198503253c030392ebbe4acfe00
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-07 15:30:11 -0800

    chore: restructure.

---
+++ sdk/cli/ffs-dev/src/cli/mod.rs
@@ -0,0 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(rename_all = "kebab-case")]
+pub enum HsmDemo {
+	#[clap(subcommand)]
+	Server(server::Server),
+}
+
+impl HsmDemo {
+	pub async fn run(&self) -> Result<(), anyhow::Error> {
+		match self {
+			HsmDemo::Server(server) => server.run().await,
+		}
+	}
+}
