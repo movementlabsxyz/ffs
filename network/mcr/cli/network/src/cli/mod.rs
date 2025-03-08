7a56656ec04eedba1c1c7474bf8a6c1ebcd9b868
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-07 16:23:31 -0800

    feat: subcommands.

--- network/mcr/cli/network/src/cli/mod.rs
+++ network/mcr/cli/network/src/cli/mod.rs
@@ -1,13 +1,44 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
+/// The `mcr-network` CLI.
 #[derive(Parser)]
 #[clap(rename_all = "kebab-case")]
-pub enum McrNetwork {}
+pub struct McrNetwork {
+	#[clap(subcommand)]
+	command: Option<McrNetworkSubcommand>,
+}
 
+/// The subcommands of the `mcr-network` CLI.
+#[derive(Subcommand)]
+#[clap(rename_all = "kebab-case")]
+pub enum McrNetworkSubcommand {
+	Run,
+}
+
+/// Implement the `From` trait for `McrNetwork` to convert it into a `McrNetworkSubcommand`.
+impl From<McrNetwork> for McrNetworkSubcommand {
+	fn from(mcr_network: McrNetwork) -> Self {
+		mcr_network.command.unwrap_or(McrNetworkSubcommand::Run)
+	}
+}
+
+/// Implement the `McrNetwork` CLI.
 impl McrNetwork {
-	pub async fn execute(&self) -> Result<(), anyhow::Error> {
-		println!("mcr-network is under development. Please check back later.");
+	pub async fn execute(self) -> Result<(), anyhow::Error> {
+		let subcommand: McrNetworkSubcommand = self.into();
+		subcommand.execute().await
+	}
+}
 
+/// Implement the `McrNetworkSubcommand` CLI.
+/// This is where the actual logic of the CLI is implemented.
+impl McrNetworkSubcommand {
+	pub async fn execute(&self) -> Result<(), anyhow::Error> {
+		match self {
+			McrNetworkSubcommand::Run => {
+				println!("mcr-network is under development. Please check back later.");
+			}
+		}
 		Ok(())
 	}
 }
