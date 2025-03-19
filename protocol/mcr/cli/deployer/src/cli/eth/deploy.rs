83def510be02924f79bf057bcbeafc19f11ccb76
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-18 22:29:20 -0700

    feat: dev deployer cli.

---
+++ protocol/mcr/cli/deployer/src/cli/eth/deploy.rs
@@ -0,0 +1,20 @@
+use anyhow::Context;
+use clap::Parser;
+use mcr_protocol_deployer_core_eth::dev::config::Config;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Serialize, Deserialize, Debug, Clone)]
+#[clap(help_expected = true)]
+pub struct Deploy {
+	#[clap(flatten)]
+	pub config: Option<Config>,
+}
+
+impl Deploy {
+	pub async fn execute(&self) -> Result<(), anyhow::Error> {
+		let config = self.config.clone().context("no config provided")?;
+		let deployer = config.build()?;
+		deployer.deploy().await?;
+		Ok(())
+	}
+}
