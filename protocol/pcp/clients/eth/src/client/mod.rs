fbd846e5bee81de99fceae98e2ed7226b78d99ea
apenzk <dr.andreas.penzkofer@gmail.com>
2025-03-20 17:41:06 +0100

    copy clients folder from mcr

---
+++ protocol/pcp/clients/eth/src/client/mod.rs
@@ -0,0 +1,294 @@
+use crate::util::send_eth_transaction::send_transaction;
+use crate::util::send_eth_transaction::PcpEthConnectorError;
+use crate::util::send_eth_transaction::VerifyRule;
+use alloy::providers::Provider;
+use alloy_primitives::Address;
+use alloy_primitives::U256;
+use alloy_sol_types::sol;
+use anyhow::Context;
+use pcp_protocol_client_core_util::{CommitmentStream, PcpClientError, PcpClientOperations};
+use pcp_types::block_commitment::{BlockCommitment, Commitment, Id};
+use serde_json::Value as JsonValue;
+use std::array::TryFromSliceError;
+use std::fs;
+use std::path::Path;
+use tokio_stream::StreamExt;
+
+// Note: we prefer using the ABI because the [`sol!`](alloy_sol_types::sol) macro, when used with smart contract code directly, will not handle inheritance.
+sol!(
+	#[allow(missing_docs)]
+	#[sol(rpc)]
+	PCP,
+	"abis/PCP.json"
+);
+
+// Note: we prefer using the ABI because the [`sol!`](alloy_sol_types::sol) macro, when used with smart contract code directly, will not handle inheritance.
+sol!(
+	#[allow(missing_docs)]
+	#[sol(rpc)]
+	MovementStaking,
+	"abis/MovementStaking.json"
+);
+
+// Note: we prefer using the ABI because the [`sol!`](alloy_sol_types::sol) macro, when used with smart contract code directly, will not handle inheritance.
+sol!(
+	#[allow(missing_docs)]
+	#[sol(rpc)]
+	MOVEToken,
+	"abis/MOVEToken.json"
+);
+
+pub struct Client<R, W> {
+	pub(crate) run_commitment_admin_mode: bool,
+	pub(crate) rpc_provider: R,
+	pub(crate) ws_provider: W,
+	pub(crate) signer_address: Address,
+	pub(crate) contract_address: Address,
+	pub(crate) send_transaction_error_rules: Vec<Box<dyn VerifyRule>>,
+	pub(crate) gas_limit: u64,
+	pub(crate) send_transaction_retries: u32,
+}
+
+impl<R, W> PcpClientOperations for Client<R, W>
+where
+	R: Provider + Clone,
+	W: Provider + Clone,
+{
+	async fn post_block_commitment(
+		&self,
+		block_commitment: BlockCommitment,
+	) -> Result<(), PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.rpc_provider);
+
+		let eth_block_commitment = PCPStorage::BlockCommitment {
+			// Currently, to simplify the API, we'll say 0 is uncommitted all other numbers are legitimate heights
+			height: U256::from(block_commitment.height()),
+			commitment: alloy_primitives::FixedBytes(
+				block_commitment.commitment().as_bytes().clone(),
+			),
+			blockId: alloy_primitives::FixedBytes(block_commitment.block_id().as_bytes().clone()),
+		};
+
+		if self.run_commitment_admin_mode {
+			let call_builder = contract.forceLatestCommitment(eth_block_commitment);
+			send_transaction(
+				self.signer_address.clone(),
+				call_builder,
+				&self.send_transaction_error_rules,
+				self.send_transaction_retries,
+				self.gas_limit as u128,
+			)
+			.await
+		} else {
+			let call_builder = contract.submitBlockCommitment(eth_block_commitment);
+			send_transaction(
+				self.signer_address.clone(),
+				call_builder,
+				&self.send_transaction_error_rules,
+				self.send_transaction_retries,
+				self.gas_limit as u128,
+			)
+			.await
+		}
+	}
+
+	async fn post_block_commitment_batch(
+		&self,
+		block_commitments: Vec<BlockCommitment>,
+	) -> Result<(), PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.rpc_provider);
+
+		let eth_block_commitment: Vec<_> = block_commitments
+			.into_iter()
+			.map(|block_commitment| {
+				Ok(PCPStorage::BlockCommitment {
+					// Currently, to simplify the API, we'll say 0 is uncommitted all other numbers are legitimate heights
+					height: U256::from(block_commitment.height()),
+					commitment: alloy_primitives::FixedBytes(
+						block_commitment.commitment().as_bytes().clone(),
+					),
+					blockId: alloy_primitives::FixedBytes(
+						block_commitment.block_id().as_bytes().clone(),
+					),
+				})
+			})
+			.collect::<Result<Vec<_>, TryFromSliceError>>()
+			.map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+
+		let call_builder = contract.submitBatchBlockCommitment(eth_block_commitment);
+
+		send_transaction(
+			self.signer_address.clone(),
+			call_builder,
+			&self.send_transaction_error_rules,
+			self.send_transaction_retries,
+			self.gas_limit as u128,
+		)
+		.await
+	}
+
+	async fn force_block_commitment(
+		&self,
+		block_commitment: BlockCommitment,
+	) -> Result<(), PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.rpc_provider);
+
+		let eth_block_commitment = PCPStorage::BlockCommitment {
+			// Currently, to simplify the API, we'll say 0 is uncommitted all other numbers are legitimate heights
+			height: U256::from(block_commitment.height()),
+			commitment: alloy_primitives::FixedBytes(
+				block_commitment.commitment().as_bytes().clone(),
+			),
+			blockId: alloy_primitives::FixedBytes(block_commitment.block_id().as_bytes().clone()),
+		};
+
+		let call_builder = contract.forceLatestCommitment(eth_block_commitment);
+		send_transaction(
+			self.signer_address.clone(),
+			call_builder,
+			&self.send_transaction_error_rules,
+			self.send_transaction_retries,
+			self.gas_limit as u128,
+		)
+		.await
+	}
+
+	async fn stream_block_commitments(&self) -> Result<CommitmentStream, PcpClientError> {
+		// Register to contract BlockCommitmentSubmitted event
+
+		let contract = PCP::new(self.contract_address, &self.ws_provider);
+		let event_filter = contract
+			.BlockAccepted_filter()
+			.watch()
+			.await
+			.map_err(|e| PcpClientError::StreamBlockCommitments(Box::new(e)))?;
+
+		let stream = event_filter.into_stream().map(|event| {
+			event
+				.and_then(|(commitment, _)| {
+					let height = commitment.height.try_into().map_err(
+						|err: alloy::primitives::ruint::FromUintError<u64>| {
+							alloy_sol_types::Error::Other(err.to_string().into())
+						},
+					)?;
+					Ok(BlockCommitment::new(
+						height,
+						Id::new(commitment.blockHash.0),
+						Commitment::new(commitment.stateCommitment.0),
+					))
+				})
+				.map_err(|err| PcpEthConnectorError::EventNotificationError(err).into())
+		});
+		Ok(Box::pin(stream) as CommitmentStream)
+	}
+
+	async fn get_commitment_at_height(
+		&self,
+		height: u64,
+	) -> Result<Option<BlockCommitment>, PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.ws_provider);
+		let PCP::getAcceptedCommitmentAtBlockHeightReturn { _0: commitment } = contract
+			.getAcceptedCommitmentAtBlockHeight(U256::from(height))
+			.call()
+			.await
+			.map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+
+		let return_height: u64 = commitment
+			.height
+			.try_into()
+			.context("failed to convert the commitment height from U256 to u64")
+			.map_err(|e| PcpClientError::Internal(e.into()))?;
+		// Commitment with height 0 mean not found
+		Ok((return_height != 0).then_some(BlockCommitment::new(
+			commitment
+				.height
+				.try_into()
+				.context("failed to convert the commitment height from U256 to u64")
+				.map_err(|e| PcpClientError::Internal(e.into()))?,
+			Id::new(commitment.blockId.into()),
+			Commitment::new(commitment.commitment.into()),
+		)))
+	}
+
+	async fn get_posted_commitment_at_height(
+		&self,
+		height: u64,
+	) -> Result<Option<BlockCommitment>, PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.ws_provider);
+		let PCP::getValidatorCommitmentAtBlockHeightReturn { _0: commitment } = contract
+			.getValidatorCommitmentAtBlockHeight(U256::from(height), self.signer_address)
+			.call()
+			.await
+			.map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+
+		let return_height: u64 = commitment
+			.height
+			.try_into()
+			.context("failed to convert the commitment height from U256 to u64")
+			.map_err(|e| PcpClientError::Internal(e.into()))?;
+
+		Ok((return_height != 0).then_some(BlockCommitment::new(
+			commitment
+				.height
+				.try_into()
+				.context("failed to convert the commitment height from U256 to u64")
+				.map_err(|e| PcpClientError::Internal(e.into()))?,
+			Id::new(commitment.blockId.into()),
+			Commitment::new(commitment.commitment.into()),
+		)))
+	}
+
+	async fn get_max_tolerable_block_height(&self) -> Result<u64, PcpClientError> {
+		let contract = PCP::new(self.contract_address, &self.ws_provider);
+		let PCP::getMaxTolerableBlockHeightReturn { _0: block_height } = contract
+			.getMaxTolerableBlockHeight()
+			.call()
+			.await
+			.map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+		Ok(block_height
+			.try_into()
+			.context("Failed to convert the max tolerable block height from U256 to u64")
+			.map_err(|e| PcpClientError::Internal(e.into()))?)
+	}
+}
+
+pub struct AnvilAddressEntry {
+	pub address: String,
+	pub private_key: String,
+}
+
+/// Read the Anvil config file keys and return all address/private keys.
+pub fn read_anvil_json_file_addresses<P: AsRef<Path>>(
+	anvil_conf_path: P,
+) -> Result<Vec<AnvilAddressEntry>, PcpClientError> {
+	let file_content =
+		fs::read_to_string(anvil_conf_path).map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+
+	let json_value: JsonValue =
+		serde_json::from_str(&file_content).map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+
+	// Extract the available_accounts and private_keys fields.
+	let available_accounts_iter = json_value["available_accounts"]
+		.as_array()
+		.expect("Available_accounts should be an array")
+		.iter()
+		.map(|v| {
+			let s = v.as_str().expect("Available_accounts elements should be strings");
+			s.to_owned()
+		});
+
+	let private_keys_iter = json_value["private_keys"]
+		.as_array()
+		.expect("Private_keys should be an array")
+		.iter()
+		.map(|v| {
+			let s = v.as_str().expect("Private_keys elements should be strings");
+			s.to_owned()
+		});
+
+	let res = available_accounts_iter
+		.zip(private_keys_iter)
+		.map(|(address, private_key)| AnvilAddressEntry { address, private_key })
+		.collect::<Vec<_>>();
+	Ok(res)
+}
---
+++ protocol/pcp/clients/eth/src/util/send_eth_transaction.rs
@@ -0,0 +1,184 @@
+use alloy::providers::Provider;
+use alloy_contract::CallBuilder;
+use alloy_contract::CallDecoder;
+use alloy_primitives::Address;
+use alloy_transport::TransportError;
+use pcp_protocol_client_core_util::PcpClientError;
+use std::marker::PhantomData;
+use thiserror::Error;
+use tracing::info;
+
+#[derive(Error, Debug)]
+pub enum PcpEthConnectorError {
+	#[error(
+		"PCP Settlement Transaction failed because gas estimation is too high. Estimated gas: {0} gas limit:{1}"
+	)]
+	GasLimitExceed(u128, u128),
+	#[error("PCP Settlement Transaction failed because account funds were insufficient: {0}")]
+	InsufficientFunds(String),
+	#[error("PCP Settlement Transaction send failed: {0}")]
+	SendTransactionError(#[from] alloy_contract::Error),
+	#[error("PCP Settlement Transaction send failed during its execution: {0}")]
+	RpcTransactionExecution(String),
+	#[error("PCP Settlement BlockAccepted event notification error: {0}")]
+	EventNotificationError(#[from] alloy_sol_types::Error),
+	#[error("PCP Settlement BlockAccepted event notification stream close")]
+	EventNotificationStreamClosed,
+}
+
+impl From<PcpEthConnectorError> for PcpClientError {
+	fn from(err: PcpEthConnectorError) -> Self {
+		PcpClientError::Internal(Box::new(err))
+	}
+}
+
+// Define a rule to verify the error generated when a transaction is send to determine if:
+// * the Transaction must me resend with more gas: return Ok(true)
+// * a specific error must be return: return Err(PcpEthConnectorError::xxx);
+// * the rule doesn't apply: return Ok(false)
+pub trait VerifyRule: Sync + Send {
+	fn verify(&self, error: &alloy_contract::Error) -> Result<bool, PcpEthConnectorError>;
+}
+
+pub struct SendTransactionErrorRule<Kind> {
+	_kind: PhantomData<Kind>,
+}
+
+impl<Kind> SendTransactionErrorRule<Kind> {
+	pub fn new() -> Self {
+		SendTransactionErrorRule { _kind: PhantomData }
+	}
+}
+
+// Define the current 2 errors managed.
+pub struct UnderPriced;
+pub struct InsufficentFunds;
+
+impl VerifyRule for SendTransactionErrorRule<UnderPriced> {
+	fn verify(&self, error: &alloy_contract::Error) -> Result<bool, PcpEthConnectorError> {
+		let alloy_contract::Error::TransportError(TransportError::ErrorResp(payload)) = error
+		else {
+			return Ok(false);
+		};
+
+		if payload.code == -32000 && payload.message.contains("transaction underpriced") {
+			Ok(true)
+		} else {
+			Ok(false)
+		}
+	}
+}
+
+impl VerifyRule for SendTransactionErrorRule<InsufficentFunds> {
+	fn verify(&self, error: &alloy_contract::Error) -> Result<bool, PcpEthConnectorError> {
+		let alloy_contract::Error::TransportError(TransportError::ErrorResp(payload)) = error
+		else {
+			return Ok(false);
+		};
+
+		if payload.code == -32000 && payload.message.contains("insufficient funds") {
+			Err(PcpEthConnectorError::InsufficientFunds(payload.message.to_string()))
+		} else {
+			Ok(false)
+		}
+	}
+}
+
+pub async fn send_transaction<P: Provider + Clone, D: CallDecoder + Clone>(
+	signer_address: Address,
+	base_call_builder: CallBuilder<(), &&P, D>,
+	send_transaction_error_rules: &[Box<dyn VerifyRule>],
+	number_retry: u32,
+	gas_limit: u128,
+) -> Result<(), PcpClientError> {
+	let base_call_builder = base_call_builder.from(signer_address);
+
+	// Fetch initial gas estimate
+	let mut estimate_gas = base_call_builder
+		.estimate_gas()
+		.await
+		.map_err(|_| PcpClientError::Internal("Gas estimation failed".into()))?
+		as u128;
+
+	// Apply an initial 20% buffer
+	estimate_gas += (estimate_gas * 20) / 100;
+	let max_gas_limit = 30_000_000; // Cap max gas to avoid runaway values
+	estimate_gas = estimate_gas.min(max_gas_limit);
+
+	info!("Initial estimated gas: {}", estimate_gas);
+
+	for attempt in 0..number_retry {
+		info!("Retry attempt: {}", attempt + 1);
+
+		// Clone the base call builder and set gas limit
+		let mut call_builder = base_call_builder.clone().gas(estimate_gas as u64);
+
+		// Fetch gas price with a 20% buffer
+		let mut gas_price = call_builder
+			.provider
+			.get_gas_price()
+			.await
+			.map_err(|e| PcpClientError::Internal(Box::new(e)))?;
+		gas_price += (gas_price * 20) / 100;
+
+		call_builder = call_builder.gas_price(gas_price);
+
+		let transaction_fee_wei = estimate_gas * gas_price;
+		if transaction_fee_wei > gas_limit {
+			return Err(PcpEthConnectorError::GasLimitExceed(transaction_fee_wei, gas_limit).into());
+		}
+
+		// Attempt to send the transaction
+		let pending_transaction = match call_builder.send().await {
+			Ok(tx) => tx,
+			Err(err) => {
+				// Apply verification rules on failure
+				for rule in send_transaction_error_rules {
+					if rule.verify(&err)? {
+						estimate_gas += (estimate_gas * 10) / 100;
+						estimate_gas = estimate_gas.min(max_gas_limit); // Prevent excessive gas increases
+						continue;
+					}
+				}
+				return Err(PcpEthConnectorError::from(err).into());
+			}
+		};
+
+		// Check transaction receipt
+		match pending_transaction.get_receipt().await {
+			Ok(receipt) if !receipt.status() => {
+				tracing::debug!(
+					"Transaction failed. Gas used: {} / Estimated gas: {}",
+					receipt.gas_used,
+					estimate_gas
+				);
+
+				let tx_gas_threshold = estimate_gas - (estimate_gas * 10) / 100;
+				if receipt.gas_used as u128 >= tx_gas_threshold {
+					tracing::info!(
+						"Transaction failed due to insufficient gas, retrying with increased gas."
+					);
+					estimate_gas += (estimate_gas * 30) / 100;
+					estimate_gas = estimate_gas.min(max_gas_limit);
+					continue;
+				} else {
+					return Err(PcpEthConnectorError::RpcTransactionExecution(format!(
+						"Transaction failed and was aborted. Receipt: {:?}",
+						receipt
+					))
+					.into());
+				}
+			}
+			Ok(_) => return Ok(()), // Transaction succeeded
+			Err(err) => {
+				return Err(PcpEthConnectorError::RpcTransactionExecution(err.to_string()).into());
+			}
+		};
+	}
+
+	// If all retries fail
+	Err(PcpEthConnectorError::RpcTransactionExecution(
+		"Transaction failed after max retries.".to_string(),
+	)
+	.into())
+}
