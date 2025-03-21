24c58fa507a038dc4da4e4f20b33b4f182c947c1
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-20 23:54:52 -0700

    feat: bring protocol cli up to ffs-dev.

---
+++ sdk/cli/ffs-dev/src/cli/mcr/mod.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+use mcr_network::cli::McrNetworkSubcommand;
+use mcr_protocol::cli::McrProtocolSubcommand;
+
+#[derive(Parser)]
+#[clap(rename_all = "kebab-case")]
+pub enum Mcr {
+	#[clap(subcommand)]
+	Network(McrNetworkSubcommand),
+	#[clap(subcommand)]
+	Protocol(McrProtocolSubcommand),
+}
+
+impl Mcr {
+	pub async fn execute(&self) -> Result<(), anyhow::Error> {
+		match self {
+			Mcr::Network(network) => {
+				network.execute().await?;
+			}
+			Mcr::Protocol(protocol) => {
+				protocol.execute().await?;
+			}
+		}
+
+		Ok(())
+	}
+}
