074809d59401befee870c1320007106d140e8847
Liam Monninger <l.mak.monninger@gmail.com>
2025-03-21 09:35:38 -0700

    feat: docgen.

--- sdk/cli/ffs-dev/src/cli/mod.rs
+++ sdk/cli/ffs-dev/src/cli/mod.rs
@@ -1,10 +1,13 @@
 pub mod mcr;
 
 use clap::Parser;
+use clap_markdown_ext::Markdown;
 
 #[derive(Parser)]
 #[clap(rename_all = "kebab-case")]
 pub enum FfsDev {
+	#[clap(subcommand)]
+	Markdown(Markdown),
 	#[clap(subcommand)]
 	Mcr(mcr::Mcr),
 }
@@ -12,6 +15,9 @@ pub enum FfsDev {
 impl FfsDev {
 	pub async fn execute(&self) -> Result<(), anyhow::Error> {
 		match self {
+			FfsDev::Markdown(markdown) => {
+				markdown.execute::<Self>().await?;
+			}
 			FfsDev::Mcr(mcr) => {
 				mcr.execute().await?;
 			}
