4a2e2ecf19e53b8e93b8fb7e1db8cda3c0c29c93
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-09 19:43:00 +0800

    fix: refactor to helper.

--- protocol/mcr/cli/client/src/cli/standard/get_accepted_commitment_at_height/mod.rs
+++ protocol/mcr/cli/client/src/cli/standard/get_accepted_commitment_at_height/mod.rs
@@ -1,9 +1,48 @@
 // ! This macro is used to generate the get_accepted_commitment_at_height command.
 // ! This is the standard implementation. However, we avoid use generics for two reasons (1) difficulties with [clap] and [serde] when using generics and (2) requiring the developer to flatten the command implementation.
 
+use clap::Parser;
+use mcr_protocol_client_core_util::{McrClientOperations, McrConfigOperations, McrViewOperations};
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Serialize, Deserialize, Debug, Clone)]
+pub struct GetAcceptedCommitmentAtHeightArgs {
+	/// The height to get the commitment for
+	#[clap(long)]
+	height: u64,
+}
+
+pub struct GetAcceptedCommitmentAtHeightHelper<V: McrViewOperations> {
+	view_config: V,
+	args: GetAcceptedCommitmentAtHeightArgs,
+}
+
+impl<V: McrViewOperations + Clone> GetAcceptedCommitmentAtHeightHelper<V> {
+	pub fn new(view_config: V, args: GetAcceptedCommitmentAtHeightArgs) -> Self {
+		Self { view_config, args }
+	}
+
+	pub async fn execute(&self) -> Result<(), anyhow::Error> {
+		let config = self.view_config.clone().try_into_config()?;
+		let client = config.build().await?;
+
+		match client.get_accepted_commitment_at_height(self.args.height).await? {
+			Some(commitment) => {
+				println!("Commitment at height {}: {:?}", self.args.height, commitment);
+			}
+			None => println!("No commitment found at height {}", self.args.height),
+		}
+
+		Ok(())
+	}
+}
+
 #[macro_export]
 macro_rules! mcr_get_accepted_commitment_at_height {
 	($view_config:ty) => {
+		use crate::cli::standard::get_accepted_commitment_at_height::{
+			GetAcceptedCommitmentAtHeightArgs, GetAcceptedCommitmentAtHeightHelper,
+		};
 		use clap::Parser;
 		use mcr_protocol_client_core_util::{
 			McrClientOperations, McrConfigOperations, McrViewOperations,
@@ -21,24 +60,17 @@ macro_rules! mcr_get_accepted_commitment_at_height {
 			#[clap(flatten)]
 			pub view_config: $view_config,
 			/// The height to get the commitment for
-			#[clap(long)]
-			height: u64,
+			#[clap(flatten)]
+			args: GetAcceptedCommitmentAtHeightArgs,
 		}
 
 		impl GetAcceptedCommitmentAtHeight {
 			pub async fn execute(&self) -> Result<(), anyhow::Error> {
-				let view_config = self.view_config.clone();
-				let config = view_config.try_into_config()?;
-				let client = config.build().await?;
-
-				match client.get_accepted_commitment_at_height(self.height).await? {
-					Some(commitment) => {
-						println!("Commitment at height {}: {:?}", self.height, commitment)
-					}
-					None => println!("No commitment found at height {}", self.height),
-				}
-
-				Ok(())
+				let helper = GetAcceptedCommitmentAtHeightHelper::new(
+					self.view_config.clone(),
+					self.args.clone(),
+				);
+				helper.execute().await
 			}
 		}
 
